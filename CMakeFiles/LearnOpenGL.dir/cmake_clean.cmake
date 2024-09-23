1078167ca7bd020d0bb51a8660f95c46e47e3a59
babai101 <deep.fatality@gmail.com>
2024-09-23 17:31:19 -0400

    initial commit

---
+++ CMakeFiles/LearnOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnOpenGL.dir/learn_opengl.cpp.o"
+  "CMakeFiles/LearnOpenGL.dir/learn_opengl.cpp.o.d"
+  "LearnOpenGL"
+  "LearnOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
